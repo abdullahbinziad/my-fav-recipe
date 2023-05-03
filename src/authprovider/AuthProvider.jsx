f52cdd7f85db764af0839046bd2ce699bbd3abe5
abdullahbinziad <abdullahbinziad@gmail.com>
2023-05-03 15:11:16 +0600

    password validation and registration page done

--- src/authprovider/AuthProvider.jsx
+++ src/authprovider/AuthProvider.jsx
@@ -1,25 +1,31 @@
-import {  createContext } from "react";
+import { createContext } from "react";
+import { getAuth, createUserWithEmailAndPassword,updateProfile  } from "firebase/auth";
+import { app } from "../firebase/Firebase.config";
 
 export const AuthContext = createContext(null);
 
+const AuthProvider = ( {children}) => {
+  const auth = getAuth(app);
 
-const AuthProvider = ({children}) => {
+  // register with email and pass
+  const register = (email, password) => {
+    return createUserWithEmailAndPassword(auth, email, password);
+  };
+  //update user info 
+  const userProfile = (name,url)=>{
+  return  updateProfile(auth.currentUser, {
+        displayName: name, photoURL: url
+      })
+  }
 
-const user ={
-    name:"Abdullah",
-    roll:13,
-}
+  const authInfo = {
+    register,
+    userProfile
+  };
 
-
-   const authInfo ={
-    user
-    }
-
-    return (
-        <AuthContext.Provider value={authInfo}>
-            {children}
-        </AuthContext.Provider>
-    );
+  return (
+    <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
+  );
 };
 
 export default AuthProvider;
--- src/components/pages/register/Register.jsx
+++ src/components/pages/register/Register.jsx
@@ -1,18 +1,42 @@
-
 import { Link } from "react-router-dom";
 
 import signUpImg from "../../../assets/imgs/login.svg";
+import { useContext, useState } from "react";
+import { AuthContext } from "../../../authprovider/AuthProvider";
 
 const Register = () => {
-   //read the auth context  
+  //read the auth context
+  const { register,userProfile } = useContext(AuthContext);
+  const [message,setMessage] = useState(<p></p>)
 
-//   handle on submit 
+  //   handle on submit
   const loginOnSubmit = (event) => {
     event.preventDefault();
     const form = event.target;
+    const name = form.name.value;
+    const photoUrl = form.photoUrl.value;
     const email = form.email.value;
     const password = form.password.value;
 
+
+    register(email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+        userProfile(name,photoUrl)
+        .then(() => {
+            setMessage( <p className="text-blue-700 font-semibold">`{user.displayName} ,Your account has Successfully Created`</p>)
+          }).catch(() => {
+          
+          });
+      })
+      .catch((error) => {
+       
+        setMessage(<p className="text-red-700 font-semibold">Something Went Wrong ! {error.message}</p>)
+        // ..
+      });
+
+      form.reset();
   };
 
   return (
@@ -24,69 +48,74 @@ const Register = () => {
         <div className="card flex-shrink-0 w-2/3  shadow-2xl bg-base-100">
           <form name="register-form" onSubmit={loginOnSubmit} className="p-12">
             <h1 className="text-4xl font-bold text-center">Sign UP Now!</h1>
-           <div className="grid grid-cols-2 gap-3">
-           <div className="form-control">
-              <label className="label">
-                <span className="label-text">Name</span>
-              </label>
-              <input
-                name="name"
-                type="text"
-                placeholder="Your Full Name"
-                className="input input-bordered"
-              />
+            <div className="grid grid-cols-2 gap-3">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Name</span>
+                </label>
+                <input
+                  name="name"
+                  type="text"
+                  placeholder="Your Full Name"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  name="email"
+                  type="email"
+                  placeholder="Email"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Confirm Password</span>
+                </label>
+                <input
+                 id="psw" pattern="(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}" title="Must contain at least one number and one uppercase and lowercase letter, and at least 6 or more characters"
+                  name="password"
+                  type="password"
+                  placeholder="Confirm password"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Photo Url</span>
+                </label>
+                <input
+                  name="photoUrl"
+                  type="text"
+                  placeholder="Photo Url"
+                  className="input input-bordered"
+                  required
+                />
+              </div>
             </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Email</span>
-              </label>
-              <input
-                name="email"
-                type="text"
-                placeholder="Email"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Confirm Password</span>
-              </label>
-              <input
-                name="password"
-                type="text"
-                placeholder="Confirm password"
-                className="input input-bordered"
-              />
-            </div>
-            <div className="form-control">
-              <label className="label">
-                <span className="label-text">Photo Url</span>
-              </label>
-              <input
-                name="photoUrl"
-                type="text"
-                placeholder="Photo Url"
-                className="input input-bordered"
-              />
-            </div>
-          
-           </div>
-           <div className="form-control mt-6 w-100">
+            <div className="form-control mt-6 w-100">
               <input
                 className="btn btn-primary w-100"
                 type="submit"
                 value="Register Now"
+
               />
             </div>
             <div className="mt-4">
-         <span className="label-text">Have already an Account?</span>
-          <Link to="/login" className=" text-blue-500 ">
-            Login
-          </Link>
-         </div>
+           {message}
+           <br />
+              <span  className="label-text  ">Have already an Account?</span>
+              <Link to="/login" className=" text-blue-500 ">
+                Login
+              </Link>
+            </div>
           </form>
-
-        
         </div>
       </div>
     </div>
